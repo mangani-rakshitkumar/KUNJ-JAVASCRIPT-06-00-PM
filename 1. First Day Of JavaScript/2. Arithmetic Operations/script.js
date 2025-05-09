0995d9c336a089957924df2fa0f78687043e23b9
mangani-rakshitkumar <manganirakshitkumar@gmail.com>
2025-05-09 10:48:27 +0530

    Add files via upload

---
+++ 1. First Day Of JavaScript/2. Arithmetic Operations/script.js
@@ -0,0 +1,37 @@
+alert("Welcome To The JavaScript")
+
+var a=10;
+console.log(a)
+
+let b=20;
+console.log(b)
+
+const c=30;
+console.log(c)
+
+// First Method Of Arithmetic Operations
+let x = prompt("Enter your value one");
+let y = prompt("Enter the two");
+
+let p = +x + +y
+let q = +x - +y
+let r = +x * +y
+let s = +x / +y
+console.log("your answer is:",+p)
+console.log("your answer is:",-q)
+console.log("your answer is:",+r)
+console.log("your answer is:",+r)
+
+// Second Method Of Arithmetic Operations
+
+let A = Number(prompt("Enter your first value :"));
+let B = Number(prompt("Enter your second value:"));
+
+let addition = A + B;
+let subtraction = A - B;
+let multiplication = A * B;
+let division = A / B;
+console.log("addition:", addition);
+console.log("subtraction:", subtraction);
+console.log("multiplication:", multiplication);
+console.log("division:", division);
