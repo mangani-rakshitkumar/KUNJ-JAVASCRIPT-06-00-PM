48be7e501494464a26ed875c04c7ea1fd7e64ea8
mangani-rakshitkumar <manganirakshitkumar@gmail.com>
2025-06-04 13:43:44 +0530

    Add files via upload

---
+++ 11. All Methods With Array - Kay And Objects/All Methods With Array - Kay And Objects.js
@@ -0,0 +1,69 @@
+let box = [
+    {
+        Name: "kumar",
+        Email: "kumar@gmail.com",
+        Password: "kumar@225"
+    },
+
+    {
+        Name: "Ava",
+        Email: "ava@gmail.com",
+        Password: "Ava123!"
+    },
+
+    {
+        Name: "Ethan",
+        Email: "ethan@gmail.com",
+        Password: "Ethan123!"
+    },
+
+    {
+        Name: "Liam",
+        Email: "liam@gmail.com",
+        Password: "Liam123!"
+    },
+
+    {
+        Name: "Noah",
+        Email: "noah@gmail.com",
+        Password: "Noah123!"
+    },
+
+    {
+        Name: "Olivia",
+        Email: "olivia@gmail.com",
+        Password: "Olivia123!"
+    },
+
+    {
+        Name: "Sophia",
+        Email: "sophia@gmail.com",
+        Password: "Sophia123!"
+    },
+
+    {
+        Name: "Mason",
+        Email: "mason@gmail.com",
+        Password: "Mason123!"
+    },
+
+    {
+        Name: "Logan",
+        Email: "logan@gmail.com",
+        Password: "Logan123!"
+    },
+
+    {
+        Name: "Aiden",
+        Email: "aiden@gmail.com",
+        Password: "Aiden123!"
+    },
+]
+
+box.forEach((a) => {
+    console.log(a);
+    console.log(a.Name);
+    console.log(a.Email);
+    console.log(a.Password);
+    console.log(a.Name , a.Email , a.Password);
+})
